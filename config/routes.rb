2f1da85529bbf4cad3ea1c7527f9a2246b90ad2c
azuesflip <azuesflip@gmail.com>
2017-09-28 13:00:42 -0600

    Set up routes to handle searches for profs, courses, and sections

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,19 @@ Rails.application.routes.draw do
   	devise_for :users
 	root "sections#index"
 	resources :sections do
-			collection do
-	      get 'search'
+		collection do
+	    	get 'search'
 	    end
 	end
-	resources :courses
-	resources :professors
+	resources :courses do
+		collection do
+	    	get 'search'
+	    end
+	end
+	resources :professors do
+		collection do
+	    	get 'search'
+	    end
+	end
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
