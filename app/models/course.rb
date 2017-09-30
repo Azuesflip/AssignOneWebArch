98870103de2c82ff84c666fc75754e0f3634f792
azuesflip <azuesflip@gmail.com>
2017-09-30 15:17:48 -0600

    added in courses validations for all of the fields

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,8 @@
 class Course < ApplicationRecord
+	validates :name, :field_of_study, :description, presence: true
+    validates :name, :field_of_study, format: { with: /\A[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]+\z/,
+    message: "Bad formatting of field" }
+    validates :name, :field_of_study, length: { in: 2..40 }
+    validates :description, length: {in: 2..2500}
 	has_many:section
 end
